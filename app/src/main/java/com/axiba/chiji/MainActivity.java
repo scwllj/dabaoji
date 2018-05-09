0ac86002b7c2afbe3ce9562e22837821e8e2929c
scwllj <33397888+scwllj@users.noreply.github.com>
2018-05-09 18:35:42 +0800

    a

--- app/src/main/java/com/axiba/chiji/MainActivity.java
+++ app/src/main/java/com/axiba/chiji/MainActivity.java
@@ -4,15 +4,29 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private AppCompatTextView refresh,home,back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refresh = findViewById(R.id.refresh);
+        if(refresh!=null)refresh.setOnClickListener(this);
+        home = findViewById(R.id.home);
+        if(home!=null)home.setOnClickListener(this);
+        back = findViewById(R.id.refresh);
+        if(back!=null)back.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }

e62d2af0281bce75c8c92cc57a53b472958576f7
scwllj <33397888+scwllj@users.noreply.github.com>
2018-07-24 18:17:33 +0800

    vvv

---
+++ app/src/zhongcaiwang/java/com/axiba/chiji/Constant.java
@@ -0,0 +1,16 @@
+package com.axiba.chiji;
+
+public class Constant {
+    public static final String START_URL="http://www-kk558.com";
+    public static final String HOME_URL="http://www-kk558.com";
+    public static final String NAVIGATION="R.layout.navigation_layout";
+    public static final boolean PULL_REFRESH=false;
+    public static final boolean SHOW_PROGRESSBAR=true;
+    public static final boolean OUTER_WEB=false;
+
+    public static final boolean NEED_GUIDE=false;
+    public static final boolean SHOW_LOADING=false;
+    public static final boolean CAN_GOBACK=true;
+    public static final boolean NEED_CACHE=true;
+
+}

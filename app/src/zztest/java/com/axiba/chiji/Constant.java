2f721da484d9fe159e94088d2f3fd97aff42509e
wl <linj@linjingdeMacBook-Air.local>
2018-08-25 21:06:14 +0800

    aaa

--- app/src/zztest/java/com/axiba/chiji/Constant.java
+++ app/src/zztest/java/com/axiba/chiji/Constant.java
@@ -1,8 +1,13 @@
 package com.axiba.chiji;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Constant {
-    public static final String START_URL = "http://sg86tl1s1iksil1lijslja.xyz/m";
-    public static final String HOME_URL = "http://sg86tl1s1iksil1lijslja.xyz/m";
+    public static final String START_URL = "http://www.baidu.com";
+    public static final String HOME_URL = "http://www.baidu.com";
     public static final String NAVIGATION = "R.layout.navigation_layout";
 
     public static final boolean PULL_REFRESH = false;
@@ -14,7 +19,11 @@ public class Constant {
     public static final boolean SHOW_LOADING = false;
     public static final boolean CAN_GOBACK = true;
     public static final boolean NEED_CACHE = true;
-    public static final String[][] popMenu = {{"濉溪县", "http://www.baidu.com"},
-                                              {"手机版", "http://www.youku.com"},
-                                              {"在线客服", "https://www.163.com/"}};
+    public static final List<PopView.Menu> popMenu = new ArrayList<>();
+    public static final Map<String, String> outerLink = new HashMap<>();
+    static {
+        popMenu.add(new PopView.Menu("主页","http://43.249.204.90:85"));
+        popMenu.add(new PopView.Menu("手机版", "http://43.249.204.90:86"));
+        popMenu.add(new PopView.Menu("在线客服", "http://43.249.204.90:8011"));
+    }
 }

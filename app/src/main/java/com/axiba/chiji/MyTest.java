05d888a72aea60abfcf4d4f27c540677d5e8846f
wl <linj@linjingdeMacBook-Air.local>
2018-08-31 00:35:45 +0800

    a

---
+++ app/src/main/java/com/axiba/chiji/MyTest.java
@@ -0,0 +1,11 @@
+package com.axiba.chiji;
+
+import com.google.common.base.Splitter;
+import com.google.common.cache.LoadingCache;
+
+public class MyTest {
+    public static void main(String[] args) {
+        Splitter.on(",");
+//        LoadingCache
+    }
+}

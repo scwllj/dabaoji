d352e448e05b3260e1ee5915d9fc4ff6363d70b7
wl <linj@linjingdeMacBook-Air.local>
2018-09-10 08:12:16 +0800

    aaa

--- app/src/main/java/com/axiba/chiji/SharedApplication.java
+++ app/src/main/java/com/axiba/chiji/SharedApplication.java
@@ -6,8 +6,8 @@ import android.util.Log;
 
 import com.tencent.smtt.sdk.QbSdk;
 
-import cn.jpush.android.api.BasicPushNotificationBuilder;
-import cn.jpush.android.api.JPushInterface;
+//import cn.jpush.android.api.BasicPushNotificationBuilder;
+//import cn.jpush.android.api.JPushInterface;
 
 public class SharedApplication extends Application {
     private final String TAG = this.getClass().getName();
@@ -19,13 +19,13 @@ public class SharedApplication extends Application {
         super.onCreate();
         instance = this;
 
-        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(this);
-        builder.statusBarDrawable = R.drawable.iconx;
-        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL;  //设置为自动消失
-        builder.notificationDefaults = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS;  // 设置为铃声与震动都要
-        JPushInterface.setPushNotificationBuilder(1, builder);
-        JPushInterface.setDebugMode(BuildConfig.DEBUG);
-        JPushInterface.init(this.getApplicationContext());
+//        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(this);
+//        builder.statusBarDrawable = R.drawable.iconx;
+//        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL;  //设置为自动消失
+//        builder.notificationDefaults = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS;  // 设置为铃声与震动都要
+//        JPushInterface.setPushNotificationBuilder(1, builder);
+//        JPushInterface.setDebugMode(BuildConfig.DEBUG);
+//        JPushInterface.init(this.getApplicationContext());
         initX5();
     }
 
--- app/src/main/java/com/axiba/chiji/receiver/MyJpushReceiver.java
+++ app/src/main/java/com/axiba/chiji/receiver/MyJpushReceiver.java
@@ -8,8 +8,8 @@ import android.util.Log;
 
 import com.axiba.chiji.MainActivity;
 
-import cn.jpush.android.api.JPushInterface;
-import cn.jpush.android.service.PushService;
+//import cn.jpush.android.api.JPushInterface;
+//import cn.jpush.android.service.PushService;
 
 
 public class MyJpushReceiver extends BroadcastReceiver {
@@ -18,40 +18,40 @@ public class MyJpushReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent pushintent=new Intent(context,PushService.class);//启动极光推送的服务
-        context.startService(pushintent);
+//        Intent pushintent=new Intent(context,PushService.class);//启动极光推送的服务
+//        context.startService(pushintent);
         try {
             Bundle bundle = intent.getExtras();
 
-            if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
-                String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
-                Log.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
-                //send the Registration Id to your server...
-
-            } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
-                Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
-//                Intent intent1 = new Intent(Constant.ACTION_JPUSH_CUSTOM);
-//                intent1.putExtras(bundle);
-//                context.sendOrderedBroadcast(intent1,null);
-            } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
-                int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
-                Log.d(TAG, "[MyReceiver] 接收到推送下来的通知的ID: " + notifactionId);
-
-            } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
-                Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
-                Intent i = new Intent(context, MainActivity.class);  //自定义打开的界面
-                i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                context.startActivity(i);
-            } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
-                Log.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
-                //在这里根据 JPushInterface.EXTRA_EXTRA 的内容处理代码，比如打开新的Activity， 打开一个网页等..
-
-            } else if(JPushInterface.ACTION_CONNECTION_CHANGE.equals(intent.getAction())) {
-                boolean connected = intent.getBooleanExtra(JPushInterface.EXTRA_CONNECTION_CHANGE, false);
-                Log.w(TAG, "[MyReceiver]" + intent.getAction() +" connected state change to "+connected);
-            } else {
-                Log.d(TAG, "[MyReceiver] Unhandled intent - " + intent.getAction());
-            }
+//            if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
+//                String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
+//                Log.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
+//                //send the Registration Id to your server...
+//
+//            } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+//                Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
+////                Intent intent1 = new Intent(Constant.ACTION_JPUSH_CUSTOM);
+////                intent1.putExtras(bundle);
+////                context.sendOrderedBroadcast(intent1,null);
+//            } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
+//                int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
+//                Log.d(TAG, "[MyReceiver] 接收到推送下来的通知的ID: " + notifactionId);
+//
+//            } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
+//                Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
+//                Intent i = new Intent(context, MainActivity.class);  //自定义打开的界面
+//                i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                context.startActivity(i);
+//            } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
+//                Log.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
+//                //在这里根据 JPushInterface.EXTRA_EXTRA 的内容处理代码，比如打开新的Activity， 打开一个网页等..
+//
+//            } else if(JPushInterface.ACTION_CONNECTION_CHANGE.equals(intent.getAction())) {
+//                boolean connected = intent.getBooleanExtra(JPushInterface.EXTRA_CONNECTION_CHANGE, false);
+//                Log.w(TAG, "[MyReceiver]" + intent.getAction() +" connected state change to "+connected);
+//            } else {
+//                Log.d(TAG, "[MyReceiver] Unhandled intent - " + intent.getAction());
+//            }
         } catch (Exception e){
 
         }

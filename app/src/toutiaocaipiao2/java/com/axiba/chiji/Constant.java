b797ebddbb3b93522664ae12f623da6988e15736
wl <linj@linjingdeMacBook-Air.local>
2019-01-01 21:43:30 +0800

    1

--- app/src/toutiaocaipiao2/java/com/axiba/chiji/Constant.java
+++ app/src/toutiaocaipiao2/java/com/axiba/chiji/Constant.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 public class Constant {
-    public static final String START_URL = "http://www.ys98988.com/m";
-    public static final String HOME_URL = "http://www.ys98988.com/m";
+    public static final String START_URL = "https://m.sahfjsdhfdjssadsttcp.com/";
+    public static final String HOME_URL = "https://m.sahfjsdhfdjssadsttcp.com/";
     public static final String NAVIGATION = "R.layout.navigation_layout";
 
     public static final boolean PULL_REFRESH = false;
@@ -27,5 +27,5 @@ public class Constant {
         popMenu.add(new PopView.Menu("主页","http://43.249.204.90:85"));
         popMenu.add(new PopView.Menu("手机版", "http://43.249.204.90:86"));
         popMenu.add(new PopView.Menu("在线客服", "http://43.249.204.90:8011"));
-    }
+        }
 }
